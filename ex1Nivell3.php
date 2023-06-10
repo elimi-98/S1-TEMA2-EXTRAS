bfda28b36e4122ff7066c8b5622a9286e1ec0672
eli-mi98 <elisiavmi@gmail.com>
2023-06-10 16:06:22 +0200

    ex1nivell3 s1 tema2

---
+++ ex1Nivell3.php
@@ -0,0 +1,24 @@
+<?php
+
+function sedasEratostenes($x){
+
+    $primos = [];
+
+    for ($i = 2; $i <= $x; $i++) {
+        if ($i != -1) {
+            $primos[] = $i;
+
+            for ($j = $i * 2; $j <= $x; $j += $i) {
+                $j = -1;
+            }
+        }
+    }
+
+    return $primos;
+}
+
+$x = 30;
+echo "Números primos hasta $x: " .sedasEratostenes($x);
+
+
+?>
